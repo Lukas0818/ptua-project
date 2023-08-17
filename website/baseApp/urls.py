ad4e600ea75997929343f9259d7e01422a1fa2f7
Lukas0818 <lukas.savickas0818@gmail.com>
2023-08-17 17:08:01 +0300

    fixed small bugs

--- website/baseApp/urls.py
+++ website/baseApp/urls.py
@@ -9,9 +9,7 @@ urlpatterns = [
     path("contact_form_submit/", views.contact_form_submit, name="contact_form_submit"),
     path("about_us/", views.about_us, name="about_us"),
     path("profile/", views.profile, name="profile"),
-    path(
-        "accounts/logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"
-    ),
+    path("accounts/logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
     path("reviews/", views.reviews, name="reviews"),
     path("reviews/create/", views.create_review, name="create_review"),
     path("reviews/delete/<int:review_id>/", views.delete_review, name="delete_review"),
